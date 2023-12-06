7169ab6f0ad30a7b3c7e78ecaac516f63d82da8a
0x-alx <alx.charbo@gmail.com>
2023-12-06 16:46:05 +0100

    Assets added

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import reactIcon from './react.png'
+import expressIcon from './express.png'
+import tailwindIcon from './tailwind.png'
+import mongodbIcon from './mongodb.png'
+import figmaIcon from './figma.png'
+import nodeIcon from './node.png'
+import viteIcon from './vite.png'
+import storybookIcon from './storybook.png'
+import typescriptIcon from './typescript.png'
+
+
+
+export { reactIcon, nodeIcon, typescriptIcon, tailwindIcon, mongodbIcon, figmaIcon, expressIcon, viteIcon, storybookIcon }
